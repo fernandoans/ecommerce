79f1836ddb770cbfe960c7d6dea5b50f88d61e11
daniel ubunutowsl <heromon.9010@gmail.com>
2023-09-23 18:03:39 -0300

    fix: warnings de default auto fild

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import dotenv
 import os
+from django.conf.global_settings import DEFAULT_AUTO_FIELD
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,6 +84,7 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
